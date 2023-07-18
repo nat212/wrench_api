86be6c0be7c594e466fa1caf163d28cfdf0207bd
Natasha Draper <natasha.draper@entelect.co.za>
2023-07-18 15:38:04 +0200

    Configure for docker

--- config/runtime.exs
+++ config/runtime.exs
@@ -48,11 +48,11 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("RENDER_EXTERNAL_HOSTNAME") || "example.com"
+  host = System.get_env("HOST") || "localhost"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :wrench_api, WrenchApiWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: host, port: port],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
