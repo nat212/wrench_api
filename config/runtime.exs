2f314e73036afab8c82eb946dcc72cfeacffe8af
Natasha Draper <natasha.draper@entelect.co.za>
2023-07-18 10:20:50 +0200

    Init distillery

--- config/runtime.exs
+++ config/runtime.exs
@@ -48,19 +48,16 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
-  port = String.to_integer(System.get_env("PORT") || "4000")
+  host = "localhost"
+  port = System.get_env("PORT") ||
+    raise """
+    environment variable PORT is missing.
+    """
 
   config :wrench_api, WrenchApiWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
-    http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
-    ],
+    http: [:inet6, port: port],
+    url: [host: host, port: port],
+    version: Application.spec(:wrench_api, :vsn),
     secret_key_base: secret_key_base
 
   # ## SSL Support
